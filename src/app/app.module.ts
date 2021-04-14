7299f8f2af33578bf51f2958cd663a09689538ea
adden00 <a.d.denisov@mail.ru>
2021-04-14 03:00:57 +0300

    убрал лишнее

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LibraryComponent } from './library/library.component';
 import { FormsModule } from '@angular/forms';
 import { TerminalButtonComponent } from './terminal-button/terminal-button.component';
-import { MainFieldComponent } from './main-field/main-field.component';
 import { PopUpMenuComponent } from './pop-up-menu/pop-up-menu.component';
 import { ElectronicComponentComponent } from './electronic-component/electronic-component.component';
-// import {DragDropModule} from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LibraryComponent,
     TerminalButtonComponent,
-    MainFieldComponent,
     TerminalButtonComponent,
     PopUpMenuComponent,
     ElectronicComponentComponent
